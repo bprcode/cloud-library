a29fe10f0cdc6b2da82cc497596f0611420d11fa
bprcode <bprcode@gmail.com>
2025-12-10 09:53:33 -0800

    Prepare swap test

--- src/index.js
+++ src/index.js
@@ -9,17 +9,24 @@ const compression = require('compression')
 // Initialize templating
 const { DateTime } = require('luxon')
 const hbs = require('hbs')
+
+// Precompilation templating
+// const Handlebars = require('handlebars')
+// const sampleTemplate = require('../built/views/partials/lean_layout')
+
+
 // TODO
 // hbs.registerPartials(path.join(__dirname, '/views/partials'))
 // hbs.registerPartials(path.join(__dirname, '/public/templates'))
-hbs.registerHelper('match', (a, b) => a === b)
-hbs.registerHelper('match-string', (a, b) => String(a) === String(b))
-hbs.registerHelper('find-in', (arr, key, value) => {
-	return arr?.find((e) => e[key] === value)
-})
-hbs.registerHelper('extract-year', (dateString) => {
-	return dateString?.match(/\d*/)[0]
-})
+// hbs.registerHelper('match', (a, b) => a === b)
+// hbs.registerHelper('match-string', (a, b) => String(a) === String(b))
+// hbs.registerHelper('find-in', (arr, key, value) => {
+// 	return arr?.find((e) => e[key] === value)
+// })
+// hbs.registerHelper('extract-year', (dateString) => {
+// 	return dateString?.match(/\d*/)[0]
+// })
+/*
 hbs.registerHelper('pretty-date', (date) => {
 	if (!date) {
 		return ''
@@ -49,33 +56,50 @@ hbs.registerHelper('error-check', (trouble, name) => {
 // Load routers
 const catalogRouter = require('./routes/catalog-route.js')
 const resetRouter = require('./routes/reset-route.js')
-
+*/
 app
-	.use(compression())
-	.use(helmet({ contentSecurityPolicy: false }))
-	.disable('x-powered-by')
+	// .use(compression())
+	// .use(helmet({ contentSecurityPolicy: false }))
+	// .disable('x-powered-by')
 
-	.use(express.urlencoded({ extended: true }))
-	.use(express.json())
+	// .use(express.urlencoded({ extended: true }))
+	// .use(express.json())
 
-	.use((req, res, next) => {
-		if (process.env.NODE_ENV !== 'production') {
-			log(req.method + '> ' + req.originalUrl, dim)
-		}
-		next()
-	})
+	// .use((req, res, next) => {
+	// 	if (process.env.NODE_ENV !== 'production') {
+	// 		log(req.method + '> ' + req.originalUrl, dim)
+	// 	}
+	// 	next()
+	// })
 
-	.use('/health', (req, res) => {
-		res.status(200).send()
-	})
-	.get('/', (req, res) => {
-		res.redirect('/catalog')
-	})
-	.use('/catalog', catalogRouter)
-	.use('/reset', resetRouter)
+	// .use('/health', (req, res) => {
+	// 	res.status(200).send()
+	// })
+	// .get('/', (req, res) => {
+	// 	res.redirect('/catalog')
+	// })
+	// .use('/catalog', catalogRouter)
+	// .use('/reset', resetRouter)
 
     // TODO
 	// .use(express.static(path.join(__dirname, 'public')))
+	.get('/hb', (req, res) => {
+		// const html = Handlebars.templates['lean_layout.hbs']()
+
+		// const justPart = html.substring(0,1023)
+
+		// const test = `${justPart}`
+		// console.log(test)
+		// res.send(test)
+		res.send('x'.repeat(1024))
+
+
+		// console.log(html)
+		// res.setHeader('Content-Type', 'text/html; charset=utf-8');
+		// res.end(Buffer.from(html, 'utf8'))
+		// res.send(`<html style="color:blue">some test string
+		// 	even with newlines and such</html>`)
+	})
 
 	.use((req, res, next) => {
 		res.status(404)
