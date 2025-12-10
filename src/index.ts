b1326bce228c9f1e680afbcdeda21367329083e2
bprcode <bprcode@gmail.com>
2025-12-09 20:58:43 -0800

    Initialize project

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,12 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { httpServerHandler } from 'cloudflare:node'
+import express from "express"
+const app = express()
 
-export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
-	},
-} satisfies ExportedHandler<Env>;
+app.use(express.json())
+
+app.get('/', (req, res) => {
+	res.json({message: 'Howdy'})
+})
+
+app.listen(3000)
+export default httpServerHandler({port: 3000})
