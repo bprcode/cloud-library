079865d85c267f599cbbc5058ca859c47e3815df
bprcode <bprcode@gmail.com>
2025-12-18 11:30:07 -0800

    port instance list, detail

--- src/controllers/hono-bookinstance-control.js
+++ src/controllers/hono-bookinstance-control.js
@@ -1,3 +1,71 @@
+import { validator } from 'hono/validator'
+import { withPagination } from '../validation/hono-pagination'
+const { paginate } = require('./paginator.js')
+const { inventory, bookInstances } = require('../database.js')
+
+const instanceIdValidator = validator('param', async (value, c) => {
+	const validated = {}
+	const instance_id = value.id?.trim()
+
+	if (!instance_id) {
+		c.trouble.add('id', 'Invalid item ID.')
+	} else {
+		validated.id = instance_id
+		const found = await inventory.find(c.client, { instance_id })
+		if (!found) {
+			c.trouble.add('id', 'Instance not found.')
+		} else {
+			validated.instance = found[0]
+		}
+	}
+
+	return validated
+})
+
 export const bookinstanceController = {
-  
+	bookinstance_create_get: [],
+	bookinstance_create_post: [],
+
+	bookinstance_delete_get: [],
+	bookinstance_delete_post: [],
+
+	bookinstance_update_get: [],
+	bookinstance_update_post: [],
+
+	bookinstance_detail: [
+		instanceIdValidator,
+		async (c) => {
+			const { instance } = c.req.valid('param')
+
+			if (!instance) {
+				return c.render(`no_results.hbs`)
+			}
+
+			return c.render(`bookinstance_detail.hbs`, {
+				book_title: instance.title, // Rename to avoid namespace conflict
+				...instance,
+			})
+		},
+	],
+
+	bookinstance_list: [
+		withPagination,
+		async (c) => {
+			const { page, limit } = c.req.valid('query')
+
+			const [itemList, total] = await Promise.all([
+				inventory.find(c.client, { page, limit }),
+
+				inventory.count(c.client),
+			])
+
+			const position = paginate(page, limit, total)
+
+			return c.render('bookinstance_list.hbs', {
+				items: itemList,
+				noResults: !itemList,
+				...position,
+			})
+		},
+	],
 }
--- src/routes/catalog-route-hono.js
+++ src/routes/catalog-route-hono.js
@@ -1,7 +1,7 @@
 import { bookController } from '../controllers/hono-book-control'
 import { genreController } from '../controllers/hono-genre-control'
-import { authorController} from '../controllers/hono-author-control'
-import { bookinstanceController} from '../controllers/hono-bookinstance-control'
+import { authorController } from '../controllers/hono-author-control'
+import { bookinstanceController } from '../controllers/hono-bookinstance-control'
 import { Hono } from 'hono'
 
 export const honoCatalogRouter = new Hono()
@@ -40,3 +40,44 @@ honoCatalogRouter
 	.post('/genre/:id/update', ...genreController.genre_update_post)
 	.get('/genre/:id', genreController.genre_detail)
 	.get('/genres', ...genreController.genre_list)
+
+	// Duplicate registrations for route aliases:
+	.get(
+		'/bookinstance/create/:id',
+		...bookinstanceController.bookinstance_create_get
+	)
+	.get('/inventory/create/:id', ...bookinstanceController.bookinstance_create_get)
+	.get('/bookinstance/create', ...bookinstanceController.bookinstance_create_get)
+	.get('/inventory/create', ...bookinstanceController.bookinstance_create_get)
+	.post('/bookinstance/create', ...bookinstanceController.bookinstance_create_post)
+	.post('/inventory/create', ...bookinstanceController.bookinstance_create_post)
+	.get(
+		'/bookinstance/:id/delete',
+		...bookinstanceController.bookinstance_delete_get
+	)
+	.get('/inventory/:id/delete', ...bookinstanceController.bookinstance_delete_get)
+	.post(
+		'/bookinstance/:id/delete',
+		...bookinstanceController.bookinstance_delete_post
+	)
+	.post(
+		'/inventory/:id/delete',
+		...bookinstanceController.bookinstance_delete_post
+	)
+	.get(
+		'/bookinstance/:id/update',
+		...bookinstanceController.bookinstance_update_get
+	)
+	.get('/inventory/:id/update', ...bookinstanceController.bookinstance_update_get)
+	.post(
+		'/bookinstance/:id/update',
+		...bookinstanceController.bookinstance_update_post
+	)
+	.post(
+		'/inventory/:id/update',
+		...bookinstanceController.bookinstance_update_post
+	)
+	.get('/bookinstance/:id', ...bookinstanceController.bookinstance_detail)
+	.get('/inventory/:id', ...bookinstanceController.bookinstance_detail)
+	.get('/bookinstances', ...bookinstanceController.bookinstance_list)
+	.get('/inventory', ...bookinstanceController.bookinstance_list)
